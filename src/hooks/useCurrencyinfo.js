a53bbac92e04fe451fb8442274a0d3e2c977c717
Ayan <mondalayan@gmail.com>
2025-05-11 19:07:15 +0530

    Implement currency converter functionality with InputBox component and useCurrencyInfo hook

--- src/hooks/useCurrencyinfo.js
+++ src/hooks/useCurrencyinfo.js
@@ -1,17 +1,16 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-    const[data, setData] = useState({});
-    useEffect(() =>{
-        fetch (`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data)
-
-
+        console.log(data);
     }, [currency])
-     console.log(data)
-     return data
+    console.log(data);
+    return data
 }
 
 export default useCurrencyInfo;
